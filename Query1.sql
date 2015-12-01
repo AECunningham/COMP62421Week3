39b7a2a1e56e3f10539f61a67beade4979d4b1e1
AECunningham <aecunningham@aecmail.plus.com>
2015-12-01 23:47:29 +0000

    Query 1

---
+++ Query1.sql
@@ -0,0 +1,38 @@
+-- (1) Simple selection query over a single relation
+
+attach database 'bbd_1.db' as 'bbd';
+select * from drinker where address = "Oxford Road";
+select * from drinker where address = "Oxford Road";
+select * from drinker where address = "Oxford Road";
+select * from drinker where address = "Oxford Road";
+detach database 'bbd';
+
+attach database 'bbd_10.db' as 'bbd';
+select * from drinker where address = "Oxford Road";
+select * from drinker where address = "Oxford Road";
+select * from drinker where address = "Oxford Road";
+select * from drinker where address = "Oxford Road";
+detach database 'bbd';
+
+attach database 'bbd_100.db' as 'bbd';
+select * from drinker where address = "Oxford Road";
+select * from drinker where address = "Oxford Road";
+select * from drinker where address = "Oxford Road";
+select * from drinker where address = "Oxford Road";
+detach database 'bbd';
+
+attach database 'bbd_1000.db' as 'bbd';
+select * from drinker where address = "Oxford Road";
+select * from drinker where address = "Oxford Road";
+select * from drinker where address = "Oxford Road";
+select * from drinker where address = "Oxford Road";
+detach database 'bbd';
+
+attach database 'bbd_10000.db' as 'bbd';
+select * from drinker where address = "Oxford Road";
+select * from drinker where address = "Oxford Road";
+select * from drinker where address = "Oxford Road";
+select * from drinker where address = "Oxford Road";
+detach database 'bbd';
+
+
